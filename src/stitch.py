69417da4a0d2101ddb906c8fe8e338d5ff7e005f
Anush Mhahajjan <anush.mahajan@research.iiit.ac.in>
2018-11-14 13:37:05 +0530

    Chages made to panorama.py

--- src/stitch.py
+++ src/stitch.py
@@ -6,27 +6,41 @@ from pyimagesearch.panorama import Stitcher
 import argparse
 import imutils
 import cv2
+from depth import find_depth
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-f", "--first", required=True,
-	help="path to the first image")
-ap.add_argument("-s", "--second", required=True,
-	help="path to the second image")
+ap.add_argument("-fc", "--firstcol", required=True,
+	help="path to the first colour image")
+ap.add_argument("-sc", "--secondcol", required=True,
+	help="path to the second colour image")
+ap.add_argument("-fd", "--firstdepth", required=True,
+	help="path to the first depth image")
+ap.add_argument("-sd", "--seconddepth", required=True,
+	help="path to the second depth image")
 args = vars(ap.parse_args())
 
-# load the two images and resize them to have a width of 400 pixels
+# load the two images and resize them to have a width of 500 pixels
 # (for faster processing)
-imageA = cv2.imread(args["first"])
-imageB = cv2.imread(args["second"])
+imageA = cv2.imread(args["firstcol"])
+imageB = cv2.imread(args["secondcol"])
+
+imageADepth = cv2.imread(args["firstdepth"],0)
+imageBDepth = cv2.imread(args["seconddepth"],0)
+
 imageA = imutils.resize(imageA, width=500)
 imageB = imutils.resize(imageB, width=500)
 
+imageADepth = imutils.resize(imageADepth, width=500)
+imageBDepth = imutils.resize(imageBDepth, width=500)
 # imageA = imageA[-250:,250:]
 # imageB = imageB[:250,:250]
 # stitch the images together to create a panorama
+
+depth = find_depth(imageBDepth)
+
 stitcher = Stitcher()
-(result, vis, imageB1) = stitcher.stitch([imageA, imageB], showMatches=True)
+(result, vis, imageB1) = stitcher.stitch(depth, [imageA, imageB], showMatches=True)
 
 # show the images
 cv2.imshow("Image A", imageA)
